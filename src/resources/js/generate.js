04d748dfbea67ce7e40e00a2c01cfb8bd7e4d5e8
Todd <todd.broll@gmail.com>
2016-12-06 14:59:50 -0600

    changed the date format and creates term options

--- src/resources/js/generate.js
+++ src/resources/js/generate.js
@@ -1,135 +1,73 @@
 window.onload = init;
 function init(){
 
-//generate date application was started
-var date = Date().split(" ");
-var month = date[1];
-var day = date[2];
-var year = date[3];
- console.log(month + " " + day + ", " + year);
+    //generate date application was started
+    var date = new Date()
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    console.log(month + "/" + day + "/" + year);
+    
+    // add the time to the test_lab_application form
 
-//generate options date of graduation
-year = parseInt(year, 10);
-var gradYears = year + 6;
-var option; 
-var text;
-var select = document.getElementById("graduationDate");
+    var currentTime = month + "/" + day + "/" + year;
+    document.getElementById("date").value = currentTime;    
 
-for(var i = year; i <=gradYears; i++){
+    //generate options date of graduation
+    year = parseInt(year, 10);
+    var gradYears = year + 6;
+    var earliestTerm = year - 6;
+    var option; 
+    var text;
 
-option = document.createElement("option");
-text = document.createTextNode("Fall " + i);
-option.appendChild(text);
-select.appendChild(option);
+    var selectGrad = document.getElementById("graduationDate");
+    var selectTerm = document.getElementById("term");
 
-option = document.createElement("option");
-var text = document.createTextNode("Spring " + i);
-option.appendChild(text);
-select.appendChild(option);
+    for(var i = year; i <=gradYears; i++){
+
+        option = document.createElement("option");
+        text = document.createTextNode("Spring " + i);
+        option.appendChild(text);
+        selectGrad.appendChild(option);
+
+        option = document.createElement("option");
+        text = document.createTextNode("Fall " + i);
+        option.appendChild(text);
+        selectGrad.appendChild(option);
+    }
+
+    // create term options for each class
+
+    var termOption;
+    for(var k = year; k >= earliestTerm; k--){
+
+        termOption = document.createElement("option");
+        text = document.createTextNode("Fall " + k);
+        termOption.appendChild(text);
+        selectTerm.appendChild(termOption);
+
+        termOption = document.createElement("option");
+        text = document.createTextNode("Spring " + k);
+        termOption.appendChild(text);
+        selectTerm.appendChild(termOption);
+    }
+
+    for(var j = 2; j < 5; j++){
+        var ClassTerm = "term" + j;
+        selectTerm = document.getElementById(ClassTerm);
+
+        for(var k = year; k >= earliestTerm; k--){
+
+            termOption = document.createElement("option");
+            text = document.createTextNode("Fall " + k);
+            termOption.appendChild(text);
+            selectTerm.appendChild(termOption);
+
+            termOption = document.createElement("option");
+            text = document.createTextNode("Spring " + k);
+            termOption.appendChild(text);
+            selectTerm.appendChild(termOption);
+        }
+    }
 }
 
-//generate number of activities fields
-
-document.getElementById("numberOfActivities").onchange = createActivityFields;
-
-document.getElementById("numberOfClasses").onchange = createClassesFields;
-}
-function createActivityFields(){
-var brk;
-var label;
-var textfield;
-var act = document.getElementById("activity");
-var activ = document.getElementById("numberOfActivities");
-var numOfActivities = activ.options[activ.selectedIndex].text;
-numOfActivities = parseInt(numOfActivities, 10);
-console.log(numOfActivities);
-for (var j = 1; j <= numOfActivities; j++){
-brk = document.createElement("br");
-act.appendChild(brk);
-
-textfield = document.createElement("input");
-textfield.type = "text";
-textfield.placeholder = "enter activity here";
-act.appendChild(textfield);
-
-textfield = document.createElement("input");
-textfield.type = "number";
-textfield.min = 0;
-textfield.max = 1200; 
-textfield.placeholder = "time(minutes) spent on activity";
-act.appendChild(textfield);
-}
-}
-
-//generate number of classes fields
-
-function createClassesFields(){
-
-var brk;
-var textfield;
-var radioButton;
-var label;
-
-var cTaken = document.getElementById("classesTaken");
-var classes = document.getElementById("numberOfClasses");
-var numOfClasses = classes.options[classes.selectedIndex].text;
-numOfClasses = parseInt(numOfClasses, 10);
-
-for (var j = 1; j <= numOfClasses; j++){
-
-brk = document.createElement("br");
-cTaken.appendChild(brk);
-
-label = document.createElement("label");
-radioButton = document.createElement("input");
-radioButton.type = "radio";
-radioButton.name = "input" + j;
-radioButton.value = "Math";
-label.appendChild(radioButton);
-label.appendChild(document.createTextNode("Math"));
-cTaken.appendChild(label);
-
-label = document.createElement("label");
-radioButton = document.createElement("input");
-radioButton.type = "radio";
-radioButton.name = "input" + j;
-radioButton.value = "Computer Science";
-label.appendChild(radioButton);
-label.appendChild(document.createTextNode("Computer Science"));
-cTaken.appendChild(label);
-
-textfield = document.createElement("input");
-textfield.type = "number";
-textfield.placeholder = "class number";
-cTaken.appendChild(label);
-cTaken.appendChild(textfield);
-
-textfield = document.createElement("input");
-textfield.type = "text";
-textfield.placeholder = "Term";
-cTaken.appendChild(textfield);
-
-textfield = document.createElement("input");
-textfield.type = "text";
-textfield.placeholder = "Instructor's name";
-cTaken.appendChild(textfield);
-
-label = document.createElement("label");
-radioButton = document.createElement("input");
-radioButton.type = "radio";
-radioButton.name = "input" + j;
-radioButton.value = "Yes";
-label.appendChild(radioButton);
-label.appendChild(document.createTextNode("Retake Yes"));
-cTaken.appendChild(label);
-
-label = document.createElement("label");
-radioButton = document.createElement("input");
-radioButton.type = "radio";
-radioButton.name = "input" + j;
-radioButton.value = "No";
-label.appendChild(radioButton);
-label.appendChild(document.createTextNode("Retake No"));
-cTaken.appendChild(label);
-}
-}
